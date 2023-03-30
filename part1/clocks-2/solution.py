ec4a89488ab44ffe90e7db695bfc44f2e1fb1191
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-03-30 13:32:59 +0300

    fix regular exp to pass September month

--- part1/clocks-2/solution.py
+++ part1/clocks-2/solution.py
@@ -2,7 +2,7 @@ import re
 
 
 def find_date(txt):
-    r1 = re.compile(r"\d{2}\/\w{3,8}\/\d{4}:\d{2}:\d{2}:\d{2}")
+    r1 = re.compile(r"\d{2}\/\w{3,9}\/\d{4}:\d{2}:\d{2}:\d{2}")
     return r1.findall(txt)
 
 
